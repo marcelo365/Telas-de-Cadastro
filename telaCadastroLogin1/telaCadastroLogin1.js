4a9b123041da25202d894093e06f5bca6a9fdfa4
marcelo365 <marcelo365@gmail.com>
2024-09-08 01:37:16 +0100

    telasCadastroAtualizacao

---
+++ telaCadastroLogin1/telaCadastroLogin1.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("container");
+
+function goLogin(){
+    container.classList.remove("active");
+}
+
+function goRegister(){
+    container.classList.add("active");
+}
